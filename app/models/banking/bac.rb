f3b6d922365106bf47437a9ffce161032bfefb4e
Emi <egonzalezm08@gmail.com>
2019-08-17 10:27:42 -0700

    Memoize to better handle bad scenario (#8)

--- app/models/banking/bac.rb
+++ app/models/banking/bac.rb
@@ -3,11 +3,11 @@
 module Banking
   class Bac
     class << self
-      URI = 'https://www.e-bac.net/sbefx/services/WebServicePublisherMessageSessionService'
-      BODY = '<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:frm="http://tempuri.org/FRM_WS_MS" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/" xsl:version="1.0" xmlns="http://tempuri.org/FRM_WS_MS"><soap:Header/><soap:Body><frm:SOAP_Domain_Msg><header><operationCode>CANALESMOVILES_Cliente_Tipo_Cambio</operationCode><origin><country>CR</country><channel>SECAN</channel><server>46.212.62.103</server><user></user><token>null</token></origin><target><country>CR</country></target></header></frm:SOAP_Domain_Msg></soap:Body></soap:Envelope>'
+      URI = ENV['BAC_URI']
+      BODY = ENV['BAC_BODY_REQUEST']
 
       def exchange_rate
-        api_client.fetch!
+        @exchange_rate ||= api_client.fetch!
       end
 
       private
