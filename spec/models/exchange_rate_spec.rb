42d1865470226937cc2f6b483a26b6d1b11c5fa7
Emi <egozalezm08@gmail.com>
2020-01-02 21:50:05 -0600

    test

--- spec/models/exchange_rate_spec.rb
+++ spec/models/exchange_rate_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe ExchangeRate, type: :model do
   subject { described_class.new(category: category, amount: amount) }
 
   let(:category) { described_class.categories[:buy] }
-  let(:amount) { 100 }
+  let(:amount) { 10 }
 
   describe 'factory' do
     it { is_expected.to be_valid }
