d45d93cd1d32cf5be41b18753a38b73e9d9a41ec
Emilio J. Gonzalez M <47124328+aka-emi@users.noreply.github.com>
2020-07-05 11:34:53 -0600

    Add.meta to incident (#67)
    * migration
    
    * comment thingy

---
+++ db/migrate/20200705171611_add_cutoffs_and_type_to_incidents.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class AddCutoffsAndTypeToIncidents < ActiveRecord::Migration[6.0]
+  def change
+    add_column :incidents, :type, :string
+    add_column :incidents, :started_at, :string
+    add_column :incidents, :ended_at, :string
+    change_column_default :incidents, :type, 'daily'
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_28_172458) do
+ActiveRecord::Schema.define(version: 2020_07_05_171611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -63,6 +63,9 @@ ActiveRecord::Schema.define(version: 2020_06_28_172458) do
     t.integer "amount", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "type", default: "daily"
+    t.string "started_at"
+    t.string "ended_at"
   end
 
 end
