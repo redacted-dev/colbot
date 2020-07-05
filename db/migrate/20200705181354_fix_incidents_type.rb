544998789b074ceb813b60915e2ce2df88c139cc
Emilio J. Gonzalez M <47124328+aka-emi@users.noreply.github.com>
2020-07-05 16:20:30 -0600

    Fix.incidents type (#70)
    * support dates within client
    
    * migrate
    
    * Revert "support dates within client"
    
    This reverts commit abb9f3fb7d91e55160c3ecd33aabc8d1ea129ce5.
    
    * cops
    
    * migrate

---
+++ db/migrate/20200705181354_fix_incidents_type.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class FixIncidentsType < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :incidents, :type
+    add_column :incidents, :category, :string
+  end
+end
